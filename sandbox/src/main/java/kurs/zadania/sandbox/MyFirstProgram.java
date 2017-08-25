2a72a4dd39289142a84ce029aaf85b147d127bcd
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-08-25 19:04:31 +0200

    constructors has been added

--- sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
@@ -5,23 +5,23 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
 
-    double l = 5;
-    System.out.println("Powierzchnia kwadratu o boku " + 1 + " = " + area(l));
+    Square s = new Square (5);
+    System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a,b));
+    Rectangle r = new Rectangle(4,6);
+
+    System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + area(r));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello," + somebody + "!");
   }
 
-  public static double area (double len) {
-    return len * len;
+  public static double area (Square s) {
+    return s.l * s.l;
   }
 
-  public static double area (double a, double b) {
-    return a * b;
+  public static double area (Rectangle r) {
+    return r.a * r.b;
   }
 }
---
+++ sandbox/src/main/java/kurs/zadania/sandbox/Square.java
@@ -0,0 +1,9 @@
+package kurs.zadania.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
