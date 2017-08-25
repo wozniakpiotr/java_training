f051f24dfb0fe53aab79c091738e9cddd8ee50d4
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-08-25 18:28:37 +0200

    some function has been added

--- sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package kurs.zadania.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+
+    double l = 5;
+    System.out.println("Powierzchnia kwadratu o boku " + 1 + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Powierzchnia prostokąta o bokach " + a + " i " + b + " = " + area(a,b));
   }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double area (double len) {
+    return len * len;
+  }
+
+  public static double area (double a, double b) {
+    return a * b;
+  }
 }
