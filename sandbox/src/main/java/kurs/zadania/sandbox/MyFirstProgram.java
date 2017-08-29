e55288369686f188130150a643703efbe02b4c20
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-08-29 18:40:03 +0200

    Function for calculation distance between two points has been created

--- sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/kurs/zadania/sandbox/MyFirstProgram.java
@@ -3,13 +3,36 @@ package kurs.zadania.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+
+    Punkt p1 = new Punkt();
+    Punkt p2 = new Punkt();
+    p1.x1 = 10;
+    p1.y1 = 10;
+    p2.x2 = 10;
+    p2.y2 = 20;
+    System.out.println("Współrzędne punktu p1 to " + "x = " + p1.x1 + " i " + "y = " + p1.y1);
+    System.out.println("Współrzędne punktu p2 to " + "x = " + p2.x2 + " i " + "y = " + p2.y2);
+    System.out.println("Odległość między punktami p1 i p2" + " = " + distance(p1,p1));
+  }
+  public static double distance(Punkt p1, Punkt p2)
+
+  {
+    return Math.sqrt((p1.x1 - p2.x2)*(p1.x1 - p2.x2)+
+            (p1.y1 - p2.y2)*(p1.y1 - p2.y2));
+
+  }
+
+}
+
+ /*
+
+  public static void main(String[] args) {
     hello("world");
 
     Square s = new Square (5);
     System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s));
 
     Rectangle r = new Rectangle(4,6);
-
     System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + area(r));
   }
 
@@ -25,3 +48,4 @@ public class MyFirstProgram {
     return r.a * r.b;
   }
 }
+ */
---
+++ sandbox/src/main/java/kurs/zadania/sandbox/Punkt.java
@@ -0,0 +1,10 @@
+package kurs.zadania.sandbox;
+
+public class Punkt {
+
+  double x1;
+  double y1;
+  double x2;
+  double y2;
+
+}
