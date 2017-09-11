6130682c99f2badb27bee7b1abde650389280c30
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-09-11 18:48:46 +0200

    method for distance calculation has been added

--- sandbox/src/main/java/kurs/zadania/sandbox/Punkt.java
+++ sandbox/src/main/java/kurs/zadania/sandbox/Punkt.java
@@ -9,4 +9,18 @@ public class Punkt {
   public double y2;
 
 }
+    double x;
+    double y;
 
+  public Punkt(double x, double y) {
+
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(Punkt p) {
+
+    return Math.sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+  }
+
+}
