46e017cfe8c4019bf761e79583b6f07d2c708997
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-09-18 19:03:37 +0200

    some tests for square area have been added

--- sandbox/src/test/java/kurs/zadania/sandbox/SquareTests.java
+++ sandbox/src/test/java/kurs/zadania/sandbox/SquareTests.java
@@ -13,3 +13,37 @@ public class SquareTests {
   }
 }
 
+/*
+test 1
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+
+    Square s = new Square(4);
+    Assert.assertEquals (s.area(), 25.0);
+  }
+}
+
+test 2
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+
+    Square s = new Square(4);
+    Assert.assertEquals (s.area(), 16.0);
+  }
+}
+
+test 3
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+
+    Square s = new Square(5);
+    Assert.assertEquals (s.area(), 30);
+  }
+}
+ */
