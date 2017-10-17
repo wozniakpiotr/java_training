69d09ebbbda90182ff178941651356d6604b4038
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-10-17 18:45:37 +0200

    GroupModificationTests class has been created

---
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package kurs.zadania.adressbook.tests;
+
+import kurs.zadania.adressbook.model.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returntoGroupPage();
+  }
+
+}
