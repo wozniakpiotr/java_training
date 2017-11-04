71de4235decb1c5e9618503cc5c4a8a474c60467
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-04 12:19:22 +0100

    Revert "NelperBase class optimalisation"
    This reverts commit 0d9f8f81b3f85fe54779dc7666edf772f7441751.

--- addressbook-web-tests/src/test/java/kurs/zadania/adressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/appmanager/HelperBase.java
@@ -13,16 +13,15 @@ public class HelperBase {
     this.wd = wd;
   }
 
-  protected void click(By locator) { wd.findElement(locator).click(); }
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
 
   protected void type(By locator, String text) {
     click(locator);
-    if (text != null) {
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
-    }
-  }
-
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
 
   public boolean isAlertPresent() {
     try {
