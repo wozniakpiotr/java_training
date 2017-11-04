272455c5dc67fec4599cf42e8dd0844130b96356
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-04 20:16:37 +0100

    Revert "Revert "Revert "checking and ensuring the performance of the preconditions...""
    This reverts commit c38b2f2a49475b52070912a8ff81e636724efa64.

--- addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupCreationTests.java
@@ -8,11 +8,7 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        // app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-        app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returntoGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
 
 }
