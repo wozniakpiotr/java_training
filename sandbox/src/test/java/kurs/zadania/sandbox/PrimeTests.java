f651d276513f6de90ac12a9a6bcde7de48b2c6f7
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-06 19:07:37 +0100

    Primes and PrimeTests classes have been added

---
+++ sandbox/src/test/java/kurs/zadania/sandbox/PrimeTests.java
@@ -0,0 +1,28 @@
+package kurs.zadania.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimes() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test(enabled = false)
+
+  public void testPrimeLong() {
+
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+
+  }
+
+}
