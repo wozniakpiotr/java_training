aa9927c773f1e977a2b141b5269b6dc98c519ab0
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-07 22:05:31 +0100

    list comparison for modified group

--- addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import kurs.zadania.adressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.security.acl.Group;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,11 +19,16 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returntoGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
