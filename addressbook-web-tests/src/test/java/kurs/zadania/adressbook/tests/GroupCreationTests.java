01541e6ba1e221a14ff6098f8d65ed4d52e13199
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-10 13:06:18 +0100

    group list sorting has been added

--- addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import kurs.zadania.adressbook.model.GroupData;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -18,9 +19,11 @@ public class GroupCreationTests extends TestBase{
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
