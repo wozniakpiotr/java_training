076e46d3767f9be10629ad7242911a8b9972d5bc
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-11 12:56:43 +0100

    comparison list for contacts - finalize

--- addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/kurs/zadania/adressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import kurs.zadania.adressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
@@ -23,6 +24,11 @@ public class ContactModificationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
 
   }
 }
