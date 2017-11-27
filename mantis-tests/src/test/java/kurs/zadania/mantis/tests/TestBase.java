0390a04a854c604bd962f55570f4f478e6a2b9be
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-27 12:30:17 +0100

    New module for Mantis test has been added

---
+++ mantis-tests/src/test/java/kurs/zadania/mantis/tests/TestBase.java
@@ -0,0 +1,23 @@
+package kurs.zadania.mantis.tests;
+
+import kurs.zadania.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+}
