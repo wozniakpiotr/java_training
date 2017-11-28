2434bf1ae77d7be90524e154223dde12db53b063
wozniakpiotr <31179454+wozniakpiotr@users.noreply.github.com>
2017-11-28 12:43:28 +0100

    login test has been added

--- mantis-tests/src/test/java/kurs/zadania/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/kurs/zadania/mantis/appmanager/ApplicationManager.java
@@ -43,4 +43,12 @@ public class ApplicationManager {
   public void stop() {
     wd.quit();
   }
+
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
---
+++ mantis-tests/src/test/java/kurs/zadania/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package kurs.zadania.mantis.tests;
+
+import kurs.zadania.mantis.appmanager.HttpSession;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
